104930b45f6ee1dab40cf25baf07de6c8483f652
Nejern <5qub6pfp@duck.com>
2024-02-02 13:39:06 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-mkdir ~/.config
+mkdir ~/.config || true
 if [[ `lsb_release -is` = "ManjaroLinux" ]]; then
     sudo pamac install --no-confirm stow
 elif [[ `lsb_release -is` = "Ubuntu" ]]; then
-    sudo aptitude update
+    sudo apt update
     sudo apt install -y aptitude
     sudo aptitude install -y stow
 fi
@@ -27,20 +27,19 @@ fi
 
 # shell
 if true; then
-    stow shell
     if [[ `lsb_release -is` = "ManjaroLinux" ]]; then
         sudo pamac install --no-confirm zsh
     elif [[ `lsb_release -is` = "Ubuntu" ]]; then
         sudo aptitude update
         sudo aptitude install -y zsh
     fi
-    sudo chsh $USER --shell=/usr/bin/zsh
-    zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+    zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    rm ~/.zshrc
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone --depth=1 https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    stow shell
 fi
 
 # git
